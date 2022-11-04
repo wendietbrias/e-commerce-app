0a2a9126c986404ae531eaa00f70517e4774aa71
wendi tbrias <wendietbrias@gmail.com>
2022-11-04 13:14:35 +0700

    tampilan product

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 import { AiOutlineHeart, AiOutlineShoppingCart } from "react-icons/ai";
 import { BsShop } from "react-icons/bs";
 import { FaShoppingCart } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-const Navbar = () => {
+const Navbar = ({ setOpen }) => {
+  const { user, loading } = useSelector((state) => state.auth);
+
   return (
     <nav className="w-full flex justify-between items-center bg-white shadow-md shadow-gray-400 py-3 px-20">
       <div className="flex items-center">
@@ -38,18 +41,24 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="w-[2px] h-5 bg-gray-400 mr-7"></div>
-        <div className="flex items-center">
-          <Link to="/login">
-            <button className="bg-button mr-3 border-2 border-button py-2 px-5 rounded-md text-white font-semibold text-sm">
-              Login
-            </button>
-          </Link>
-          <Link to="/register">
-            <button className="border-2 border-button py-2 px-5 rounded-md text-button font-semibold text-sm">
-              Register
-            </button>
-          </Link>
-        </div>
+        {user ? (
+          <div className="flex items-center">
+            <h2>Login as user</h2>
+          </div>
+        ) : (
+          <div className="flex items-center">
+            <Link to="/login">
+              <button className="bg-button mr-3 border-2 border-button py-2 px-5 rounded-md text-white font-semibold text-sm">
+                Login
+              </button>
+            </Link>
+            <Link to="/register">
+              <button className="border-2 border-button py-2 px-5 rounded-md text-button font-semibold text-sm">
+                Register
+              </button>
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,92 @@
 import { useState } from "react";
 import { Navbar, Footer } from "../components";
 import { useSelector } from "react-redux";
+import { AiOutlineSearch, AiOutlineHeart, AiFillStar } from "react-icons/ai";
+import { ProductCard } from "../components";
+
+const datadummy = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const WrapperModal = ({ children, CloseModal }) => {
+  return (
+    <div
+      onClick={CloseModal}
+      className="fixed top-0 left-0 w-full h-screen bg-wrapper"
+    >
+      {children}
+    </div>
+  );
+};
 
 const Home = () => {
   const { user } = useSelector((state) => state.auth);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [openStoreModal, setOpenStoreModal] = useState(true);
+  const [openCartModal, setOpenCartModal] = useState(false);
+
+  const CloseModalStore = (e) => {
+    if (e.target.className.includes("fixed")) {
+      setOpenStoreModal(false);
+    }
+  };
+
+  const CloseModalCart = (e) => {
+    if (e.target.className.includes("fixed")) {
+      setOpenCartModal(false);
+    }
+  };
+
   return (
     <section className="flex flex-col justify-between min-h-screen w-full">
-      <Navbar />
+      <Navbar
+        setOpenCartModal={setOpenCartModal}
+        setOpenStoreModal={setOpenStoreModal}
+      />
+      <div className="w-full pb-10">
+        <img src="assets/event.jpg" alt="event" className="w-full" />
+        <form className="w-full flex justify-center my-10">
+          <div className="rounded-full flex items-center overflow-hidden shadow-md shadow-slate-400 px-2">
+            <AiOutlineSearch className="text-lg" />
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="outline-none px-2 w-[500px] bg-white py-2"
+            />
+          </div>
+        </form>
+        <div className=" px-20 grid grid-cols-5 gap-4">
+          {datadummy?.map((product, idx) => (
+            <ProductCard key={idx} product={product} />
+          ))}
+        </div>
+      </div>
       <Footer />
+      {openStoreModal && (
+        <WrapperModal CloseModal={CloseModalStore}>
+          {user ? (
+            <div className="absolute top-20 right-24 bg-white py-4 rounded-md px-8 text-center">
+              <h2 className="text-lg font-bold text-title">
+                Anda Belum Memiliki Toko
+              </h2>
+              <button className="w-full mt-3 bg-button py-2 px-5 rounded-md text-sm text-white font-medium">
+                Login
+              </button>
+            </div>
+          ) : (
+            <div className="absolute top-20 right-24 bg-white py-4 rounded-md px-8 text-center">
+              <h2 className="text-lg font-bold text-title">Anda Belum Login</h2>
+              <button className="w-full mt-3 bg-button py-2 px-5 rounded-md text-sm text-white font-medium">
+                Login
+              </button>
+            </div>
+          )}
+        </WrapperModal>
+      )}
+      {openCartModal && (
+        <WrapperModal CloseModal={CloseModalCart}>
+          <div className="w-full">Cart</div>
+        </WrapperModal>
+      )}
     </section>
   );
 };
